a8b9c008a66acf9dc3d9075cc6c21337b15dd8f4
Gulam Anas <g.anas786@gmail.com>
2022-04-10 00:12:01 +0530

    added number field for weight

--- lib/home.dart
+++ lib/home.dart
@@ -12,7 +12,39 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Planet X'),
+        title: Text('Weight on Planet X'),
+        centerTitle: true,
+        backgroundColor: Colors.black38,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: ListView(
+          padding: const EdgeInsets.all(2.5),
+          children: [
+            Image.asset("assets/planet.png",
+            height: 133.0,
+            width: 200.0,),
+
+            Container(
+              margin: const EdgeInsets.all(3.0),
+              alignment: Alignment.center,
+              child: Column(
+                children: [
+                  TextField(
+                    controller: null,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Your Weight on Earth',
+                      hintText: 'In Pounds',
+                      icon: Icon(Icons.person_outline),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
