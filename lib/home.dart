5b799a2a574db137d6dd567baa0f299fcd4d31ce
Gulam Anas <g.anas786@gmail.com>
2022-04-10 13:11:29 +0530

    Radio buttons

--- lib/home.dart
+++ lib/home.dart
@@ -8,6 +8,16 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  int radioValue = 0;
+
+  void handleRadioValueChanged( value) {
+    setState(() {
+      radioValue = value;
+      // print(radioValue);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +49,33 @@ class _HomeState extends State<Home> {
                       hintText: 'In Pounds',
                       icon: Icon(Icons.person_outline),
                     ),
+                  ),
+                  Padding(padding: EdgeInsets.all(5.0)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Radio<int>(value: 0, groupValue: radioValue, onChanged: handleRadioValueChanged, activeColor: Colors.brown,),
+                      Text(
+                        'Pluto',
+                        style: TextStyle(
+                          color: Colors.white30
+                        ),
+                      ),
+                      Radio<int>(value: 1, groupValue: radioValue, onChanged: handleRadioValueChanged, activeColor: Colors.red,),
+                      Text(
+                        'Mars',
+                        style: TextStyle(
+                          color: Colors.white30
+                        ),
+                      ),
+                      Radio<int>(value: 2, groupValue: radioValue, onChanged: handleRadioValueChanged, activeColor: Colors.orangeAccent,),
+                      Text(
+                        'Venus',
+                        style: TextStyle(
+                          color: Colors.white30
+                        ),
+                      ),
+                    ],
                   )
                 ],
               ),
